0e05e715325a024f2f2f9eece01452ae50b29209
saumya gupta <saumya.gupta_cs18@gla.ac.in>
2020-02-24 17:55:28 +0530

    take input of lastname and make while loop and use regex

--- contactlist/src/definition/Contactoptions.java
+++ contactlist/src/definition/Contactoptions.java
@@ -20,6 +20,16 @@ public class Contactoptions {
             }
             firstName = scanner.nextLine().trim();
         }
+        System.out.println("Last Name : ");
+        String lastName = scanner.nextLine().trim();
+        while (true) {
+            if (lastName.matches("[A-Za-z]+")) {
+                break;
+            } else {
+                System.out.println("Please enter correct name");
+            }
+            lastName = scanner.nextLine().trim();
+        }
 
     }
 }
