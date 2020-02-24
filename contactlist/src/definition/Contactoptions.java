c40bc69525eb49a871c8f7b2d5dc0d3bbf1bb04c
saumya gupta <saumya.gupta_cs18@gla.ac.in>
2020-02-24 17:46:23 +0530

    make scanner and object of linkedlist

---
+++ contactlist/src/definition/Contactoptions.java
@@ -0,0 +1,9 @@
+package definition;
+
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class Contactoptions {
+    Scanner scanner = new Scanner(System.in);
+    LinkedList<String> linkedList = new LinkedList<>();
+}
