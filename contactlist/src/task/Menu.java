aa9fb25042b3254d2f71cfc83f92613ba5478b07
saumya gupta <saumya.gupta_cs18@gla.ac.in>
2020-02-24 22:01:20 +0530

    make switch case for output

--- contactlist/src/task/Menu.java
+++ contactlist/src/task/Menu.java
@@ -1,6 +1,45 @@
 package task;
 
 
-public class Menu {
+import contactlinkedlist.Linkedlist1;
 
+import java.util.Scanner;
+
+public class Menu {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Linkedlist1 linkedList1 = new Linkedlist1();
+        Contactoptions tasks = new Contactoptions();
+        System.out.println("Welcome to DBC's Contact List App");
+        System.out.println("Press 1 to add a new contact");
+        System.out.println("Press 2 to view all contacts");
+        System.out.println("Press 3 to search for a contact");
+        System.out.println("Press 4 to delete a contact");
+        System.out.println("Press 5 to exit program ");
+        int choice = Integer.parseInt(scanner.nextLine());
+        if (choice == 1) {
+            tasks.addContact(linkedList1);
+            choice = Integer.parseInt(scanner.nextLine());
+        }
+        if (choice == 2) {
+            tasks.viewcontact(linkedList1);
+            choice = Integer.parseInt(scanner.nextLine());
+        }
+        if (choice == 3) {
+            tasks.searchContact(linkedList1);
+            choice = Integer.parseInt(scanner.nextLine());
+        }
+        if (choice == 4) {
+            tasks.deleteContact(linkedList1);
+            choice = Integer.parseInt(scanner.nextLine());
+        }
+        if (choice == 5) {
+            System.out.println("App terminate");
+        }
+    }
 }
+
+
+
+
+
